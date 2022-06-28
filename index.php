e40dbdd1cbe76a308f07a5efb0655faa4e242d52
Louis Velasco <21021463+loiSvelasco@users.noreply.github.com>
2022-06-28 14:03:16 +0800

    Initial commit

---
+++ index.php
@@ -0,0 +1,43 @@
+<?php
+
+function adminer_object()
+{
+    // Required to run any plugin.
+    include_once "./plugins/plugin.php";
+
+    // Plugins auto-loader.
+    foreach (glob("plugins/*.php") as $filename) {
+        include_once "./$filename";
+    }
+
+    // Specify enabled plugins here.
+    $plugins = [
+        new AdminerDatabaseHide([
+			"mysql",
+			"information_schema",
+			"performance_schema",
+			"phpmyadmin",
+		]),
+		
+        new AdminerLoginServers([
+            filter_input(INPUT_SERVER, 'HTTP_HOST') => filter_input(INPUT_SERVER, 'SERVER_NAME')
+        ]),
+		
+        new AdminerTablesFilter(),
+        new AdminerSimpleMenu(),
+        new AdminerCollations(),
+        new AdminerJsonPreview(),
+
+        // AdminerTheme has to be the last one.
+        new AdminerTheme("default-blue"),
+		// Color variant can by specified in constructor parameter.
+		// new AdminerTheme("default-orange"),
+		// new AdminerTheme("default-blue"),
+		// new AdminerTheme("default-green", ["192.168.0.1" => "default-orange"]),
+    ];
+
+    return new AdminerPlugin($plugins);
+}
+
+// Include original Adminer or Adminer Editor.
+include "./adminer.php";
